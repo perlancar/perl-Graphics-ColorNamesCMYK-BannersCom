b78fe5d837dfc6be9b9b589d32e567557490ce4d
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2024-05-06 14:31:35 +0700

    Initial commit

---
+++ lib/Graphics/ColorNamesCMYK/BannersCom.pm
@@ -0,0 +1,41 @@
+package Graphics::ColorNamesCMYK::BannersCom;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our $NAMES_CMYK_TABLE = {
+    'navy blue' => 0x64320041, # 100,50,0,65
+    'dark blue' => 0x644b0000, # 100,75,0,0
+    'light blue' => 0x46250d00, # 70,37,13,0
+    'dark gray' => 0x0000004b, # 0,0,0,75
+    'medium gray' => 0x0000003c, # 0,0,0,60
+    'light gray' => 0x0000002d, # 0,0,0,45
+    'dark green' => 0x64196419, # 100,25,100,25
+    'green' => 0x4b006419, # 75,0,100,25
+    'orange' => 0x0048bc00, # 0,70,700,0
+    'red' => 0x00645a05, # 0,100,90,5
+    'deep red' => 0x19646419, # 25,100,100,25
+    'magenta' => 0x00640000, # 0,100,0,0
+    'purple' => 0x55640000, # 85,100,0,0
+    'violet' => 0x2a320000, # 42,50,0,0
+    'pink' => 0x00320000, # 0,50,0,0
+    'teal' => 0x64193219, # 100,25,50,25
+    'brown' => 0x1d4e614b, # 29,78,97,75
+    'white' => 0x00000000, # 0,0,0,0
+};
+
+1;
+# ABSTRACT: Basic CMYK colors from banners.com
+
+=head1 DESCRIPTION
+
+
+=head1 SEE ALSO
+
+L<https://www.banners.com/resources/basic-cmyk-colors-for-printing-banners>
+
+Other C<Graphics::ColorNamesCMYK::*> modules.
